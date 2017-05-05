33c35dad2fa0edaaf9f6d31bba0287fd06437b73
Ian Main <imain@redhat.com>
2017-05-05 13:49:11 -0400

    Almost good to go I think..
    Added yum update with commit.
    Added process pool.
    Basically fixed a bunch of stuff. :)

--- container-check.py
+++ container-check.py
@@ -26,18 +26,19 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
+
 def parse_opts(argv):
     parser = argparse.ArgumentParser("Tool to let you know what packages need"
                                      "updating in a list of containers")
     parser.add_argument('-c', '--containers',
                         help="""File containing a list of containers to inspect.""",
                         default='container_list')
+    parser.add_argument('-p', '--process-count',
+                        help="""Number of processes to use in the pool when running docker containers.""",
+                        default=multiprocessing.cpu_count())
     parser.add_argument('-r', '--rpm-list',
                         help="""File containing a list of the latest available rpms.""",
                         default="rpm_list")
-    parser.add_argument('-y', '--yum-config',
-                        help="""Alternate yum configuration file.""",
-                        default=False)
     parser.add_argument('-u', '--update',
                         action='store_true',
                         help="""Run yum update in any containers that need updating.""",
@@ -45,6 +46,7 @@ def parse_opts(argv):
     opts = parser.parse_args(argv[1:])
 
     return opts
+
 
 def rm_container(name):
     log.info('Removing container: %s' % name)
@@ -59,29 +61,72 @@ def rm_container(name):
            'No such container: {}\n'.format(name):
         log.debug(cmd_stderr)
 
-def run_container_cmd(container, name, command):
 
-    rm_container('package-check-%s' % name)
+def populate_container_rpms_list((container)):
 
     dcmd = ['/usr/bin/docker', 'run',
             '--user', 'root',
-            '--name', 'package-check-%s' % name,
+            '--rm',
             container]
 
-    dcmd.extend(command)
+    dcmd.extend(['rpm', '-qa'])
 
-    log.debug('Running docker command: %s' % ' '.join(dcmd))
+    log.info('Running docker command: %s' % ' '.join(dcmd))
 
     subproc = subprocess.Popen(dcmd, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+                               stderr=subprocess.PIPE)
     cmd_stdout, cmd_stderr = subproc.communicate()
-    if cmd_stderr:
-        log.debug(cmd_stderr)
     if subproc.returncode != 0:
-        log.error('Failed running docker-puppet.py for %s' % config_volume)
-    rm_container('package-check-%s' % name)
+        log.error('Failed running rpm -qa for %s' % container)
+        log.error(cmd_stderr)
 
-    return cmd_stdout
+    rpms = cmd_stdout.split("\n")
+
+    return (subproc.returncode, container, rpms)
+
+
+def yum_update_container((container, name)):
+
+    container_name = 'yum-update-%s' % name
+
+    rm_container(container_name)
+
+    dcmd = ['/usr/bin/docker', 'run',
+            '--user', 'root',
+            '--name', container_name,
+            container]
+
+    dcmd.extend(['yum', '-y', 'update'])
+
+    log.info('Running docker command: %s' % ' '.join(dcmd))
+
+    subproc = subprocess.Popen(dcmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    cmd_stdout, cmd_stderr = subproc.communicate()
+    if subproc.returncode != 0:
+        log.error('Failed running yum update for %s' % container)
+        log.error(cmd_stderr)
+        rm_container(container_name)
+        return (subproc.returncode, container)
+
+    dcmd = ['/usr/bin/docker', 'commit',
+            '-m', 'automatic yum update',
+            container_name,
+            container]
+
+    log.info('Running docker command: %s' % ' '.join(dcmd))
+
+    subproc = subprocess.Popen(dcmd, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    cmd_stdout, cmd_stderr = subproc.communicate()
+    if subproc.returncode != 0:
+        log.error('Failed running docker commit in %s' % container)
+        log.error(cmd_stderr)
+
+    rm_container(container_name)
+
+    return (subproc.returncode, container)
+
 
 def get_available_rpms():
     available_rpms = {}
@@ -91,28 +136,65 @@ def get_available_rpms():
 
     return available_rpms
 
-opts = parse_opts(sys.argv)
 
-# Load up available rpms as a hash containing the latest versions of rpms.
-available_rpms = get_available_rpms()
+if __name__ == '__main__':
+    opts = parse_opts(sys.argv)
 
-# Get a list of all the docker containers we need to inspect.
-docker_containers = [line.rstrip('\n') for line in open(opts.containers)]
+    # Load up available rpms as a hash containing the latest versions of rpms.
+    available_rpms = get_available_rpms()
 
-process_count = int(os.environ.get('PROCESS_COUNT',
-                                   multiprocessing.cpu_count()))
+    # Get a list of all the docker containers we need to inspect.
+    docker_containers = [line.rstrip('\n') for line in open(opts.containers)]
 
-container_rpms = {}
-name = 0
-for container in docker_containers:
-    rpm_qa = run_container_cmd(container, str(name), ['rpm', '-qa'])
-    rpms = rpm_qa.split("\n")
-    container_rpms[container] = rpms
-    name += 1
+    # Holds all the information for each process to consume.
+    # Instead of starting them all linearly we run them using a process
+    # pool.
+    process_map = []
+    for container in docker_containers:
+        process_map.append(container)
 
-for container in container_rpms:
-    for rpm in container_rpms[container]:
-        if len(rpm) > 0 and not rpm in available_rpms:
-            log.info("Should update rpm: %s container: %s\n" % (rpm, container))
-#print container_rpms
+    # This is what we're after here, a hash keyed by containers, each entry
+    # containing a list of rpms in that container.
+    container_rpms = {}
+    success = True
+    # Fire off processes to perform each rpm list.
+    p = multiprocessing.Pool(int(opts.process_count))
+    ret = list(p.map(populate_container_rpms_list, process_map))
+    for returncode, container, rpms in ret:
+        container_rpms[container] = rpms
+        if returncode != 0:
+            log.error('ERROR running rpm query in container: %s' % container)
+            success = False
 
+    if not success:
+        sys.exit(1)
+
+    container_update_list = {}
+
+    for container in container_rpms:
+        for rpm in container_rpms[container]:
+            if len(rpm) > 0 and not rpm in available_rpms:
+                if container not in container_update_list:
+                    container_update_list[container] = []
+                container_update_list[container].append(rpm)
+
+    for container in container_update_list:
+        print("Container needs updating: %s" % container)
+        for rpm in container_update_list[container]:
+            print("  rpm: %s" % rpm)
+
+    # And finally update the containers if requried
+    if opts.update:
+        process_map = []
+        name = 0
+        for container in container_update_list:
+            process_map.append([container, str(name)])
+            name += 1
+
+        ret = list(p.map(yum_update_container, process_map))
+        for returncode, container in ret:
+            if returncode != 0:
+                log.error('ERROR running yum update in container %s' % container)
+                success = False
+        if not success:
+            sys.exit(1)
