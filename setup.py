1a1ca4cbee892556050ceb12ebaeca579fbc67c6
Ian Main <imain@redhat.com>
2017-08-31 18:36:40 -0400

    Apache license actually.  Also no default for containers file.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     author_email='imain@redhat.com',
 
     # Choose your license
-    license='GPLv3',
+    license='Apache Software License',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -45,7 +45,7 @@ setup(
         'Topic :: System :: Archiving :: Packaging',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: Apache Software License'
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
