c7e4ed53b6158cd71c35c031f880413b8ed8c023
Ian Main <imain@redhat.com>
2017-08-31 18:31:25 -0400

    yum just has to be on the system I guess..

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyum', 'multiprocessing', 'logging', 'argparse'],
+    install_requires=['multiprocessing', 'logging', 'argparse'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
